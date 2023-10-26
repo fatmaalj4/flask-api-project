2b6650f9399150fcc59388b4450b0b77f6f372c8
fatmaalj4 <fatmafatouma98@icloud.com>
2023-10-25 23:57:01 -0400

    routes

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
-# app.py
-from flask import Flask
+from app import create_app
+from app.routes import bp
 
-app = Flask(__name__)
+app = create_app()
 
-def create_app():
-    # Configurations et enregistrements d'autres blueprints
-    return app
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run()
--- app/routes.py
+++ app/routes.py
@@ -15,8 +15,13 @@ def get_db_connection():
     )
     return conn
 
-@bp.route('/api', methods=['GET'])
+@bp.route('/topn', methods=['GET'])
 def get_top_n():
+    top_n = request.args.get('n', type=int, default=10)
+    
+    # Valider que topN est entre 1 et 100
+    if not 1 <= top_n <= 100:
+        return jsonify({"error": "Parameter n should be between 1 and 100"}), 400
     
     with get_db_connection() as conn:
         with conn.cursor() as cur:
@@ -24,13 +29,18 @@ def get_top_n():
                 SELECT crawl_view.*, score_view.score 
                 FROM crawl_view 
                 JOIN score_view ON crawl_view.id = score_view.entity_id
-                ORDER BY score_view.score DESC''')
+                ORDER BY score_view.score DESC
+                LIMIT %s
+            ''', (top_n,))
             
             rows = cur.fetchall()
-            
+            columns = [desc[0] for desc in cur.description]
+            result = [dict(zip(columns, row)) for row in rows]
 
-    return jsonify(rows)
+    return jsonify(result)
 
+app.register_blueprint(bp, url_prefix='/api')
 
 if __name__ == '__main__':
     app.run(debug=True)
+ 
